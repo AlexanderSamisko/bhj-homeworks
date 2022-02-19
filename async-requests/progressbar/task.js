ccf0df0cf622c0e845f4f52120b752f46ee4e434
AlexanderSamisko <83254509+AlexanderSamisko@users.noreply.github.com>
2022-02-19 13:48:34 +0300

    homework v1.1

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,23 +1,12 @@
-const requestForm = new XMLHttpRequest();
-requestForm.open(`POST`, `https://netology-slow-rest.herokuapp.com/upload.php`);
-
 let form = document.querySelector(`#form`);
-let postingForm = new FormData(form);
-
-requestForm.send(postingForm);
-
-
-let moveProgress = function() {
-    const progress = document.getElementById( 'progress' );
-    if (this.readyState === 1) {
-        progress.value = 0.2;
-    } else if (this.readyState === 2) {
-        progress.value = 0.6;
-    } else if (this.readyState === 3) {
-        progress.value = 0.95;
-    } else if (this.readyState === 4) {
-        progress.value = 1;
-    }
-}
-
-requestForm.addEventListener(`readystatechange`, moveProgress);
+form.addEventListener(`submit`, (evt)=> {
+    const postingForm = new FormData(form);
+    const requestForm = new XMLHttpRequest();
+    requestForm.open(`POST`, `https://netology-slow-rest.herokuapp.com/upload.php`);
+        requestForm.upload.onprogress = function(event) {
+            const progress = document.getElementById( 'progress' );
+            progress.value = event.loaded/event.total;
+        }
+    requestForm.send(postingForm);
+    evt.preventDefault();
+})
