9caa32f25c22c9e809ed9a54f505cca0807abcbc
AlexanderSamisko <83254509+AlexanderSamisko@users.noreply.github.com>
2022-02-23 12:16:31 +0300

    homework v 1.1

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,12 +1,21 @@
 const modalWindow = document.querySelector(`#subscribe-modal`);
 
 
-modalWindow.classList.add("modal_active");
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
 
+const wasHereWindow = getCookie(`action`);
 
-const closeModal = modalWindow.querySelector(`.modal__close_times`);
-closeModal.addEventListener(`click`, ()=> {
+if (!wasHereWindow) {
+    modalWindow.classList.add("modal_active");
+    const closeModal = modalWindow.querySelector(`.modal__close_times`);
+    closeModal.addEventListener(`click`, ()=> {
     document.cookie = 'action=Close; max-age=360000000';
     modalWindow.classList.remove("modal_active");  
 })
+}
 
